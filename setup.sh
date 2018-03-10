f183a28664633675143374160085e50a9af3f3b9
Paweł Kołodziejski <aquadran@users.sourceforge.net>
2018-03-10 05:47:56 +0100

    remove gcut

--- setup.sh
+++ setup.sh
@@ -29,7 +29,7 @@ get_os() {
 }
 
 gnutools="[ awk b2sum base32 base64 basename cat chcon chgrp chmod chown chroot cksum comm cp \
-csplit cut date dd df dir dircolors dirname du echo env expand expr factor false fmt fold \
+csplit date dd df dir dircolors dirname du echo env expand expr factor false fmt fold \
 groups head hostid id install join kill link ln logname ls md5sum mkdir mkfifo mknod mktemp \
 mv nice nl nohup nproc numfmt od paste pathchk pinky pr printenv printf ptx pwd readlink \
 realpath rm rmdir runcon sed seq sha1sum sha224sum sha256sum sha384sum sha512sum shred shuf \
